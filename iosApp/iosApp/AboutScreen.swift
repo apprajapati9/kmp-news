03e3ffb74f1caab4e5afa00b3ca4b09fdbfc9f58
Ajay P. Prajapati <apprajapati@hotmail.com>
2024-09-11 13:31:08 -0400

    added aboutscreen view for ios in swift

---
+++ iosApp/iosApp/AboutScreen.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct AboutScreen : View {
+    var body: some View{
+        NavigationStack{
+            AboutListView().navigationTitle("About Device- Ajay")
+        }
+    }
+}
+
+#Preview {
+    AboutScreen()
+}
